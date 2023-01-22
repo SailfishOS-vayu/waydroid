c60a95f3d27a3abff7b79ce2cb3f01e439d9554c
Alessandro Astone <ales.astone@gmail.com>
2023-01-22 16:13:57 +0100

    upgrade: Fallback to stopping container manually

--- tools/actions/upgrader.py
+++ tools/actions/upgrader.py
@@ -41,6 +41,7 @@ def upgrade(args):
             container.Stop(False)
         except Exception as e:
             logging.debug(e)
+            tools.actions.container_manager.stop(args)
     migration(args)
     helpers.drivers.loadBinderNodes(args)
     if not args.offline:
